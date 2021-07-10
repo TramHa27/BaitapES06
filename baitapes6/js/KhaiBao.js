61106e5bc8a7d7c64e9008e086f2e876ac690f18
Trâm Hà <hatranbichtram94@gmail.com>
2021-07-10 21:19:22 +0700

    'version1'

---
+++ baitapes6/js/KhaiBao.js
@@ -0,0 +1,16 @@
+// export class KhaiBao{
+//     ten = '';
+//     ho = '';
+//     email = '';
+//     soDienThoai = '';
+//     ngay = '';
+//     thang = '';
+//     nam = '';
+//     diaChi = '';
+//     thongTinDiChuyen = '';
+//     loaiNhiem = '';
+//     constructor() {
+
+//     }
+// }
+
---
+++ baitapes6/js/ListKhaiBao.js
@@ -0,0 +1,15 @@
+// import { KhaiBao } from "./KhaiBao.js";
+
+// document.querySelector(".signup-form .actions ul li:last-child a").onclick =
+//   function () {
+//     let khaiBao = new KhaiBao();
+//     let arrTagInput = document.querySelector(
+//       ".form-group input, .form-date input, .form-group select"
+//     );
+//     for (let input of arrTagInput) {
+//       let name = input.getAttribute("name");
+//       let value = input.value;
+//       khaiBao[name] = value
+//     }
+//     console.log("arrtag", arrTagInput);
+//   };
